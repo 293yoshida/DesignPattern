8b47fa4eeeef7e09f8f82c86e40e2eb8d1809ff3
yoshida <yoshida@yoshida-PC>
2015-02-21 22:42:00 +0900

    add student class and studentlist class

--- DesignPattern/src/IteratorPattern.java
+++ DesignPattern/src/IteratorPattern.java
@@ -14,7 +14,44 @@ public class IteratorPattern {
 
 
 class Student{
-	public Student() {
+	private String name = "";
+	private int sex = 0;
+
+	public Student(String name, int sex) {
+		// TODO 自動生成されたコンストラクター・スタブ
+		this.name = name;
+		this.sex  = sex;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public int getSex(){
+		return this.sex;
+	}
+}
+
+class StudentList{
+	private Student[] students;
+	private int last = 0;
+
+	public StudentList() {
 		// TODO 自動生成されたコンストラクター・スタブ
 	}
+
+	public StudentList(int studentCount) {
+		// TODO 自動生成されたコンストラクター・スタブ
+		this.students = new Student[studentCount];
+	}
+
+	public void add(Student student){
+		students[last] = student;
+		last++;
+	}
+
+	public int size(){
+		return this.last;
+	}
+
 }
